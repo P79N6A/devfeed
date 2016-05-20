6eef4f94a0178fd5b67ac5e2abb5bc92cce6a142
kairee <kaireewu@gmail.com>
2016-05-21 01:58:35 +0800

    不使用laravel 的资源预编译，清理环境 增加系统所需的数据表 migrate 文件

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,7 +11,7 @@
 |
 */
 
-$factory->define(Fedn\User::class, function (Faker\Generator $faker) {
+$factory->define(Fedn\Models\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
