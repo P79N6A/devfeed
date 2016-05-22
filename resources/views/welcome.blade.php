2325fc186ab323b0ce57e9e4d5311880cdd6f37e
kairee <kaireewu@gmail.com>
2016-05-22 19:15:31 +0800

    fix default template.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <title>Laravel</title>
 
-        <link href="{{ elixir('css/app.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
 
         <style>
             html, body {
