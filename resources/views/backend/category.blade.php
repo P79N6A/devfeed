19b472193f91a7ff0aedb007089dff491395479c
kairee <kaireewu@gmail.com>
2016-06-21 19:37:28 +0800

    feat(Article): continue working on category template.

--- resources/views/backend/category.blade.php
+++ resources/views/backend/category.blade.php
@@ -3,5 +3,35 @@
 @section('pageTitle','分类管理')
 
 @section('content')
-<h1 class="page-header">分类管理 <a href="{{ url('admin/category/create') }}" data-toggle="modal" data-target="#categoryModal" class="btn btn-primary">新建分类</a></h1>
+<h1 class="page-header">分类管理</h1>
+<div class="row">
+    <div class="col-md-4">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">添加分类</h3>
+            </div>
+            <div class="panel-body">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">名称</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputFile">File input</label>
+                        <input type="file" id="exampleInputFile">
+                        <p class="help-block">Example block-level help text here.</p>
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-8">
+
+    </div>
+</div>
 @endsection
