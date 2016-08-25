02d42c200cffa84c14b8e1c621de4680a4e4c090
kairee <kaireewu@gmail.com>
2016-08-25 16:14:50 +0800

    feat(Auth): 验证发表和修改文章权限

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Fedn\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+
+class Admin
+{
+
+    protected $auth;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  Guard $auth
+     * @return void
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($this->auth->guest()) {
+            if ($request->ajax()) {
+                return response('Unauthorized.', 401);
+            } else {
+                return redirect()->guest('login');
+            }
+        }
+
+        if($request->user()->hasRoles([1,2,3])) {
+            return $next($request);
+        }
+
+        return response('Unauthorized.', 403);
+    }
+}
