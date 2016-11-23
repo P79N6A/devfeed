147f57a821991005e78f97a1898128de85477188
kairee <kaireewu@gmail.com>
2016-11-23 12:07:23 +0800

    Feat(Article): 优化“猜你喜欢”的文章筛选逻辑.优化文章缓存

--- app/Http/Controllers/Front/ArticleController.php
+++ app/Http/Controllers/Front/ArticleController.php
@@ -8,13 +8,23 @@ use Fedn\Http\Requests;
 use Fedn\Http\Controllers\Controller;
 
 use Fedn\Models\Article;
-
+use Cache;
 class ArticleController extends Controller
 {
     public function view($id) {
-        $newArticle = Article::where('id', '!=', $id)->orderBy('created_at')->take(5)->get();
-        $likeArticle = Article::where('id', '!=', $id)->orderBy(\DB::raw('RAND()'))->take(5)->get();
-        $article = Article::with('tags')->findOrFail($id);
+        $newArticle = Cache::tags('articles')->remember('newest_5', 30, function() use ($id) {
+            return Article::where('id', '!=', $id)->latest()->take(5)->get();
+        });
+        //$likeArticle = Article::where('id', '!=', $id)->orderBy(\DB::raw('RAND()'))->take(5)->get();
+        $article = Cache::remember('article_'.$id, 30, function() use ($id) {
+            return Article::with('tags')->findOrFail($id);
+        });
+
+        $likeArticle = Article::whereHas('tags', function($query) use ($article) {
+            $query->whereIn('title', $article->tags->pluck('title'));
+        })->where('id', '!=', $id)->latest('click_count')->take(5)->get();
+
+        $article->timestamps = false;
         $article->click_count += 1;
         $article->save();
 
