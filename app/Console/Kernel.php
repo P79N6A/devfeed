2ae7b3ed87fdb7b97d64db1210c05e3f943123a3
kairee <kaireewu@gmail.com>
2016-11-30 04:31:43 +0800

    Feat(Spider): 把采集时间改为凌晨5点.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('fedn:fetch')
-                 ->dailyAt('3:00');
+                 ->dailyAt('5:00');
     }
 
     /**
