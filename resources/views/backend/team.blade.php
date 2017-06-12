486180f2c4fec8dddb866c51dead2ef4d571f8cf
kairee <kaireewu@gmail.com>
2017-06-13 02:14:40 +0800

    add team default view file.

--- resources/views/backend/team.blade.php
+++ resources/views/backend/team.blade.php
@@ -1,5 +1,13 @@
 @extends('backend.layout')
 
 @section('content')
-    <h3>sss</h3>
+    <h3 class="page-header">团队管理</h3>
+    <div class="row col-md-12" id="teamApp">
+
+    </div>
+@endsection
+
+@section('pageScript')
+  {{ Html::script('/js/vue.min.js') }}
+  {{ Html::script('/js/team.js') }}
 @endsection
