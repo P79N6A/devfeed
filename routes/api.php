785b719eaccc4e509f4624de11c001487f7afe5d
kairee <kaireewu@gmail.com>
2017-07-20 14:55:08 +0800

    refactor(framework): Update framework to 5.4.30.
    Close #43
    
    Signed-off-by: kairee <kaireewu@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,10 @@ use Illuminate\Http\Request;
 
 Route::group(['prefix' => 'v1', 'as' => 'api.'], function () {
 
-    Route::get('/user', function (Request $request) {
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
         return $request->user();
-    })->middleware('auth:api');
+    });
+
 
     Route::group(['namespace' => 'Common'], function () {
         Route::post('upload/{type}', ['as' => 'upload', 'uses' => 'FileController@upload'])
