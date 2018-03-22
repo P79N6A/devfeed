fc17de31189cfe1bf604ec5191aa089f839a697f
kairee <364298+krwu@users.noreply.github.com>
2018-03-22 18:22:00 +0800

    调整单元测试“

--- tests/Unit/ImageFetchTest.php
+++ tests/Unit/ImageFetchTest.php
@@ -13,6 +13,7 @@ use Illuminate\Http\UploadedFile;
 
 class ImageFetchTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
@@ -22,31 +23,27 @@ class ImageFetchTest extends TestCase
     {
         $testHtml = <<<'EOF'
 <div>
-    <img src="https://file.ofcdn.com/attachment/1.png">
-    <img src="http://file.ofcdn.com/uploads/201801/2.png?_v=20324234">
-    <img src="//file.ofcdn.com/uploads/3.png">
-    <img src="/images/4.png">
+    <img src="https://file.ofcdn.com/attachments/8638990568.png">
+    <img src="http://file.ofcdn.com/upfiles/ds1.png?_v=20324234">
+    <img src="http://file.ofcdn.com/uploads/201801/no_exist.png?_v=20324234">
+    <img src="//file.ofcdn.com/promo/cdn.jpg" width="336" height="280" style="display:inline;magin:0;height:auto">
+    <img src="/attachments/2011/03/QQ.png">
     <img src="attachments/5.png">
 </div>
 EOF;
         $baseUrl = 'https://ofcss.com/2018/01/02/hello.html';
         $html = phpQuery::newDocumentHTML($testHtml);
-        $images = $html->find('img');
+        $images = collect([]);
+        $html->find('img')->map(function($pqo) use (&$images){
+            $url = $pqo->getAttribute('src');
+            if(empty($url) === false) {
+                $images->push($url);
+            }
+        });
 
-        //todo complete this test.
-        //$this->markTestIncomplete('依赖测试未完成，此测试已暂时中止');
-
-        $result = ImageUtil::fetchImages($images, $baseUrl);
-
-        $this->assertArrayHasKey('https://file.ofcdn.com/attachment/1.png', $result);
-        $this->assertArrayHasKey('http://file.ofcdn.com/uploads/201801/2.png?_v=20324234', $result);
-        $this->assertArrayHasKey('//file.ofcdn.com/uploads/3.png', $result);
-        $this->assertArrayHasKey('/images/4.png', $result);
-        $this->assertArrayHasKey('attachments/5.png', $result);
-
-        $this->assertEquals('https://file.ofcdn.com/attachment/1.png', $result['https://file.ofcdn.com/attachment/1.png']['remote']);
-        $this->assertEquals('https://ofcss.com/2018/01/02/attachments/5.png', $result['attachments/5.png']['remote']);
-        $this->assertEquals('https://ofcss.com/images/4.png', $result['/images/4.png']['remote']);
+        $result = ImageUtil::fetchImages($images, $baseUrl, true);
+        $this->assertCount(4, $result);
+        $this->assertInstanceOf(RemoteFile::class, $result->first());
     }
 
 
