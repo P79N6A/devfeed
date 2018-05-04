991655af54002b8eaf95519777d5d9d0f52aee0a
lyushine <lyushine@qq.com>
2018-05-04 20:51:01 +0800

    删除报错HEADER

--- resources/assets/js/app/front.js
+++ resources/assets/js/app/front.js
@@ -8,13 +8,13 @@ import routes from './routes';
 if('undefined' === typeof window.axios) {
     window.axios = require('axios');
 }
-let token = document.head.querySelector('meta[name="csrf-token"]');
+// let token = document.head.querySelector('meta[name="csrf-token"]');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
+// if (token) {
+//     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+// } else {
+//     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+// }
 
 
 const HOST = 'http://www.gohosts.com/';
