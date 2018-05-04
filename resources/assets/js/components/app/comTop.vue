dd4a857ad344cedd3ebcea550d349878c65c9734
lyushine <lyushine@qq.com>
2018-05-04 09:49:24 +0800

    增加登录态

--- resources/assets/js/components/app/comTop.vue
+++ resources/assets/js/components/app/comTop.vue
@@ -3,13 +3,19 @@
         <a class="uk-button" data-uk-offcanvas="{target:'#sideMenu'}"><i class="show-btn spr hide"></i></a>
         <h1 class="logo"><a href="/" title="DevFeed" class="hide">DevFeed</a></h1>
         <div class="login">
-            <div class="unlogin"><a href="/login" class="spr">登录</a></div>
+            <div v-if="loginStatus" class="unlogin" id="unlogin"><a href="/login" class="spr">登录</a></div>
+            <div v-if="!loginStatus" class="logined" id="logined" style="display:none"><a href="/auth/logout" class="spr">注销</a></div>
         </div>
     </div>
 </template>
 <script>
     export default {
         name: "comTop",
+        data:function(){
+            return {
+                loginStatus:!!window.status
+            }
+        },
         computed: {},
         methods: {}
     }
